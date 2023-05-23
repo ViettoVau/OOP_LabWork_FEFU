fb0299c615e6cac12da80e3fdaee1aec6fe47241
ViettoVau <101384855+ViettoVau@users.noreply.github.com>
2023-05-23 16:15:02 +1000

    Add files via upload

---
+++ Memento и ranges.find.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+class Memento {
+public:
+    virtual std::string GetName() const = 0;
+    virtual std::string GetDate() const = 0;
+};
+
+class ConcreteMemento : public Memento {
+private:
+    std::string state_;
+    std::string date_;
+
+public:
+    ConcreteMemento(std::string state) : state_(state) {
+        std::time_t now = std::time(0);
+        this->date_ = now;
+    }
+
+    std::string GetState() const {
+        return state_;
+    }
+
+    std::string GetName() const override {
+        return this->state_;
+    }
+
+    std::string GetDate() const override {
+        return this->date_;
+    }
+};
+
+class Math {
+private:
+    std::string state_;
+
+    int printGeometricProgression(int a, int r, int n) {
+        for (int i = 0; i < n; i++) {
+            std::cout << a << " ";
+            a *= r;
+        }
+        return a;
+    }
+
+public:
+    Math(std::string state) : state_(state) {
+        std::cout  << this->state_ << "\n";
+    }
+
+    void DoSomething(int a, int r, int n) {
+        std::cout << "Doing...  \n";
+        this->state_ = std::to_string(this->printGeometricProgression(a, r, n));
+        std::cout << "Result:  " << this->state_ << "\n";
+    }
+
+    Memento* Save() {
+        return new ConcreteMemento(this->state_);
+    }
+
+    void Restore(Memento* memento) {
+        this->state_ = dynamic_cast<ConcreteMemento*>(memento)->GetState();
+        std::cout << "Return state: " << this->state_ << "\n";
+    }
+};
+
+class Caretaker {
+private:
+    std::vector<Memento*> mementos_;
+    Math* math_;
+
+public:
+    Caretaker(Math* math) : math_(math) {}
+
+    void Backup() {
+        std::cout << "\nSaving Math's state...\n";
+        this->mementos_.push_back(this->math_->Save());
+    }
+
+    void Undo(const std::string& name) {
+        std::vector<Memento*>::iterator iter = FindMemento(name);
+        if (iter != mementos_.end()) {
+            Memento* memento = *iter;
+            std::cout << "Restoring state to: " << memento->GetName() << "\n";
+            try {
+                math_->Restore(memento);
+                mementos_.erase(iter);
+            }
+            catch (...) {
+                Undo(name);
+            }
+        }
+    }
+
+    void ShowHistory() const {
+        std::cout << "The list of mementos:\n";
+        for (Memento* memento : this->mementos_) {
+            std::cout << memento->GetName() << "\n";
+        }
+    }
+
+    std::vector<Memento*>::iterator FindMemento(const std::string& name) {
+        return std::ranges::find_if(this->mementos_, [&name](Memento* m) { return m->GetName() == name; });
+    }
+    const std::vector<Memento*>& GetMementos() const {
+        return this->mementos_;
+    }
+};
+
+int main() {
+    Math* math = new Math("State 1");
+    Caretaker* caretaker = new Caretaker(math);
+
+    caretaker->Backup();
+    math->DoSomething(2, 3, 5);
+    caretaker->Backup();
+    math->DoSomething(1, 2, 7);
+    caretaker->Backup();
+    math->DoSomething(5, 2, 4);
+
+    std::cout << "\n";
+    caretaker->ShowHistory();
+
+    std::cout << "\n";
+    caretaker->Undo("State 1");
+    return 0;
+}
+
---
+++ Observer.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <vector>
+
+class Observer {
+public:
+	virtual void update(const std::string& message) = 0;
+};
+
+class Video {
+	std::string title;
+	int likes = 0;
+	int dislikes = 0;
+
+public:
+	Video(const std::string& title) : title(title) {}
+
+	const std::string& getTitle() const {
+		return title;
+	}
+	void like() {
+		likes++;
+	}
+
+	void dislike() {
+		dislikes++;
+	}
+
+	void printStats() const {
+		std::cout  << "Статистика видео \"" << title << "\":" << std::endl;
+		std::cout << "Лайки: " << likes << std::endl;
+		std::cout << "Дизлайки: " << dislikes << std::endl;
+	}
+};
+
+// Класс Блогер
+class Blogger {
+	std::string name;
+	std::vector<Observer*> observers;
+	std::vector<Video> videos;
+
+public:
+	Blogger(const std::string& name) : name(name) {}
+
+	void addObserver(Observer* observer) {
+		observers.push_back(observer);
+	}
+
+	void removeObserver(Observer* observer) {
+		auto it = std::find(observers.begin(), observers.end(), observer);
+		if (it != observers.end()) {
+			observers.erase(it);
+		}
+	}
+
+	void notifyAll(const std::string& message) {
+		for (auto observer : observers) {
+			observer->update(message);
+		}
+	}
+
+	void publishPost(const std::string& message) {
+		std::cout << name << " опубликовал новую запись: " << message << std::endl;
+		notifyAll(message);
+	}
+
+	void loadVideo(const Video& video) {
+		videos.push_back(video);
+		std::string message = "Новое видео: " + video.getTitle();
+		std::cout << name << " загрузил новое видео: " << video.getTitle() << std::endl;
+		notifyAll(message);
+	}
+};
+
+class Subscriber : public Observer
+{
+	std::string name;
+
+public:
+	Subscriber(const std::string& name) : name(name) {}
+
+	void update(const std::string& message) 
+	{
+		std::cout << name << " получил уведомление: " << message << std::endl;
+	}
+	void likeVideo(Video& video) {
+		video.like();
+		video.printStats();
+	}
+
+	void dislikeVideo(Video& video) {
+		video.dislike();
+		video.printStats();
+	}
+};
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Blogger blogger("Юлия Высоцкая");
+	Subscriber subscriber1("ТУтуту");
+	Subscriber subscriber2("Трутрутурутр");
+
+	blogger.addObserver(&subscriber1);
+	blogger.addObserver(&subscriber2);
+	blogger.publishPost("Новый рецептик вышел!");
+
+	Video video("Рецепт прекрысных помидорчиков<3");
+
+	blogger.loadVideo(video);
+	subscriber1.likeVideo(video);
+	subscriber2.dislikeVideo(video);
+	blogger.removeObserver(&subscriber2);
+	
+	blogger.publishPost("Еще один рецептик!");
+
+	return 0;
+}
